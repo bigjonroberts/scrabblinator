7ffa6c47c4b038e3556e8cfe9520eaac3222e680
Jon Roberts <bigjonroberts@gmail.com>
2018-09-27 10:27:38 -0500

    initial commit converting to Scrabblinator

--- README.md
+++ README.md
@@ -1,40 +1,23 @@
-# Glossiator
-[Glossary micro service](https://hub.docker.com/r/dustinmoris/glossiator/) with the intention to integrate it with Slack's [Slash Commands](https://api.slack.com/slash-commands) API.
+# Scrabblinator
+[Scrabblinator micro service](https://hub.docker.com/r/bigjonroberts/scrabblinator) with the intention to integrate it with Slack's [Slash Commands](https://api.slack.com/slash-commands) API.
 
-[![Build Status](https://travis-ci.org/dustinmoris/Glossiator.svg)](https://travis-ci.org/dustinmoris/Glossiator)
-
-[![Build History](https://buildstats.info/travisci/chart/dustinmoris/Glossiator)](https://travis-ci.org/dustinmoris/Glossiator/builds)
+It was copied from [Glossiator](https://hub.docker.com/r/dustinmoris/glossiator/) for the core Slack parsing and response.
 
 ## How it works
 
 ### Run the Docker container
 
-[Glossiator is a micro service](https://hub.docker.com/r/dustinmoris/glossiator/) which can be hosted in your own environment by running the following Docker command:
+[Scrabblinator is a micro service](https://hub.docker.com/r/dustinmoris/glossiator/) which can be hosted in your own environment by running the following Docker command:
 
 ```
-docker run -d -p 8083:8083 -e URL_OR_PATH_TO_CSV="{path to csv file}" -e TOKEN="{secret token}" dustinmoris/glossiator:latest
+docker run -d -p 8083:8083 -e PREFIX="scrabble-" -e GENMODE="emoji" -e TOKEN="{secret token}" dustinmoris/glossiator:latest
 ```
 
-The Docker container must be launched with two environment variables:
+The Docker container must be launched with at least one environment variable:
 
-- URL_OR_PATH_TO_CSV
 - TOKEN
 
-`URL_OR_PATH_TO_CSV` must be an absolute local path or a web URL to a CSV file which contains all the glossary terms. `TOKEN` is the secret token that you will be given from Slack's [Slash Commands](https://api.slack.com/slash-commands) configuration page (see below).
-
-The CSV file is expected to have three columns:
-
-- Term
-- Meaning
-- Description
-
-Term is usually the key or an abbreviation that your team would be searching for. Meaning is a short description and Description is a more detailed explanation.
-
-| Example | |
-| :--- | :--- |
-| Term: | CI |
-| Meaning: | Continuous Integration |
-| Description: | Continuous Integration (CI) is a development practice that requires developers to integrate code into a shared repository several times a day. Each check-in is then verified by an automated build, allowing teams to detect problems early. |
+`TOKEN` is the secret token that you will be given from Slack's [Slash Commands](https://api.slack.com/slash-commands) configuration page (see below).
 
 #### Optional parameters
 
@@ -47,15 +30,15 @@ The default value is set to `2`.
 The next step is to integrate it with Slack's [Slash Commands](https://api.slack.com/slash-commands) API.
 
 1. Click on the "Add Configuration" button to add a new configuration.
-2. Enter the desired command. e.g. `/whatis`
-3. Enter the URL to the self hosted Glossiator app. The correct endpoint is called `/whatis`.
-  - Example: `https://glossiator.my-server.com/whatis`
+2. Enter the desired command. e.g. `/scrabble`
+3. Enter the URL to the self hosted scrabblinator app. The correct endpoint is called `/scrabble`.
+  - Example: `https://scrabblinator.my-server.com/scrabble`
 4. Choose `POST` from the method drop down.
 5. Use the auto-generated token from the Token field to configure the Docker container (see above).
 6. Configure the remaining fields to your likes. Pick a name and icon for the Slack bot and fill in the optional help text.
 7. Click "Save Integration" and the setup is complete!
 
-Now you should be able to go into your Slack channel and be able to type `/whatis some-term` and get a successful response from the Glossiator app.
+Now you should be able to go into your Slack channel and be able to type `/scrabble hello` and get a successful response from the Scrabblinator app.
 
 ## License
 
---
+++ Scrabblinator/Program.fs
@@ -0,0 +1,172 @@
+﻿open System
+open System.Configuration
+open System.Net
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.RequestErrors
+open System.Text.RegularExpressions
+
+// -------------------------------
+// CONFIG
+// -------------------------------
+
+module Config =
+    let fallback     nextOption value   = if value = null then nextOption   else value    
+    let fallbackFunc nextOption value   = if value = null then nextOption() else value
+    let fallbackToError msg             = fallbackFunc (fun _ -> failwith msg)
+
+    let getValue key = 
+        Environment.GetEnvironmentVariable key
+        |> fallback (ConfigurationManager.AppSettings.[key])
+        |> fallbackToError (sprintf "The config setting with the name %s could not be found." key)
+
+    let getValueOrDefault key default1 = 
+        Environment.GetEnvironmentVariable key
+        |> fallback (ConfigurationManager.AppSettings.[key])
+        |> fallback default1
+
+// -------------------------------
+// SCRABBLETEXT DOMAIN
+// -------------------------------
+
+module ScrabbleText =
+
+    let prefix = Config.getValueOrDefault "PREFIX" ""    
+
+    let port = Config.getValueOrDefault "PORT" "8083" |> uint16
+
+    type GenMode =
+      | Emoji
+      | Image
+
+    let genMode =
+        match (Config.getValueOrDefault "GENMODE" "Emoji").ToLower() with
+        | "image" -> Image
+        | _ -> Emoji
+
+    // stolen from http://www.fssnip.net/29/title/Regular-expression-active-pattern
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
+    let asciiChar = 
+        seq { 65 .. 90 }
+        |> Seq.append (seq { 97 .. 121 })
+        |> Seq.map char
+        |> Set.ofSeq
+
+    let scrabbleize genMode (text:string) =
+    //   let toEmoji (c:char) =
+    //     match string c with
+    //     //   | Regex @"^\p{L}*$" chr -> List.head chr
+    //       | Regex @"^[a-zA-Z]*$" chr -> chr
+    //       | _ -> [ "blank" ]
+    //     |> List.tryHead
+    //     |> function | Some a -> a | None -> "blank"
+    //     |> sprintf ":%s%s:" prefix
+      let toEmoji (c:char) = 
+        match Set.contains c asciiChar with
+          | true -> sprintf ":%s%c:" prefix c
+          | false -> sprintf ":%sblank:" prefix
+      match genMode with
+        | Emoji -> 
+            let emojiText = text |> Seq.map toEmoji 
+            let emojiList = Seq.toList emojiText
+            String.concat " " emojiList
+        | Image -> "not implemented yet"
+
+// -------------------------------
+// SLACK INTEGRATION
+// -------------------------------
+
+module SlackIntegration =
+
+    let token = Config.getValue "TOKEN"
+
+    type SlackRequest =
+        {
+            Token       : string
+            TeamId      : string
+            TeamDomain  : string
+            ChannelId   : string
+            ChannelName : string
+            UserId      : string
+            UserName    : string
+            Command     : string
+            Text        : string
+            ResponseUrl : string
+        }
+        static member FromHttpContext (ctx : HttpContext) =
+            let get key =
+                match ctx.request.formData key with
+                | Choice1Of2 x  -> x
+                | _             -> ""
+            printfn "%A" ctx.request
+            {
+                Token       = get "token"
+                TeamId      = get "team_id"
+                TeamDomain  = get "team_domain"
+                ChannelId   = get "channel_id"
+                ChannelName = get "channel_name"
+                UserId      = get "user_id"
+                UserName    = get "user_name"
+                Command     = get "command"
+                Text        = get "text"
+                ResponseUrl = get "response_url"
+            }
+
+    type ValidationResponse<'T> =
+        | Valid     of 'T
+        | Invalid   of string
+
+    let validateRequest (slackRequest : SlackRequest) =
+        if slackRequest.Token = token
+        then Valid slackRequest
+        else Invalid "Invalid token in request. Your Slack team is not permitted to use this service."        
+
+    let SLACK_RESPONSE (text : string) =
+        text.Replace("&", "&amp;")
+            .Replace("<", "&lt;")
+            .Replace(">", "&gt;")
+        |> sprintf "{ \"response_type\": \"in_channel\", \"text\": \"%s\"}"
+        |> OK
+        >=> Writers.setMimeType "application/json"
+
+    let slackCommand (f : SlackRequest -> string) =
+        fun (ctx : HttpContext) ->
+            printfn "context:%A" ctx.request.rawForm
+            (match SlackRequest.FromHttpContext ctx |> validateRequest with
+            | Invalid msg -> BAD_REQUEST msg
+            | Valid   req -> f req |> SLACK_RESPONSE) ctx
+
+// -------------------------------
+// WEB SERVICE
+// -------------------------------
+
+open ScrabbleText
+open SlackIntegration
+
+let scrabble (req : SlackRequest) = scrabbleize genMode req.Text
+
+let testit = scrabbleize genMode "damnit"
+
+let app = 
+    choose [ 
+        GET  >=> OK "Scrabblinator version 0.1.0-beta"
+        POST >=>
+            choose [
+                path "/scrabble"  >=> slackCommand scrabble
+                path "/scrabble/" >=> slackCommand scrabble ]
+        NOT_FOUND "Resource not found. Please note that URLs are case sensitive." ]
+
+let config =
+    { defaultConfig with
+        bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") port ] }
+
+[<EntryPoint>]
+let main argv =
+    startWebServer config app
+    0
